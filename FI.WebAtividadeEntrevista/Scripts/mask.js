e0f8a190d809148f41564e9b82563cd52c3b88b7
emanuelhm <miyagawa.emanuel@gmail.com>
2023-03-24 16:41:23 -0300

    Adicionando campo de CPF e sua máscara à View "Cliente.Form"

---
+++ FI.WebAtividadeEntrevista/Scripts/mask.js
@@ -0,0 +1,11 @@
+﻿window.addEventListener('load', function () {
+
+    let cpfs = this.document.querySelectorAll('[data-mask-type="cpf"]');
+
+    for (let cpf of cpfs) {
+        IMask(cpf, {
+            mask: '000.000.000-00'
+        });
+    }
+
+}, false);
